7d32fe2a325c67f17ebca321f4ad031ab18c7bae
thounilo <thounilo@gmail.com>
2020-07-24 17:45:50 +0300

    added buttonGroup

---
+++ ButtonGroup/uiButtonGroup.vue
@@ -0,0 +1,40 @@
+<style lang="scss">
+
+  //* First and second element gets global border-radius
+
+  .ui-button-group {
+
+    --border-radius: var(--ui-border-radius-lg, .25em);
+
+    display: flex;
+
+    & button:first-child {
+      border-radius: var(--border-radius) 0 0 var(--border-radius);
+    }
+    & button:last-child {
+      border-radius: 0 var(--border-radius) var(--border-radius) 0;
+    }
+    & button:not(:first-child):not(:last-child) {
+      border-radius: 0;
+    }
+  }
+
+</style>
+
+
+<template>
+
+  <div class="ui-button-group">
+    <slot></slot>
+  </div>
+
+</template>
+
+
+<script>
+
+  export default {
+    name: "ui-button-group"
+  }
+
+</script>
