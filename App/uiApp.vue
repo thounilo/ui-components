583267a6f3b817ca43e01e331c6e53a48b10e94d
thounilo <thounilo@gmail.com>
2020-07-31 04:19:30 +0300

    Fixed mobile vh to ignore url field at top

--- App/uiApp.vue
+++ App/uiApp.vue
@@ -3,16 +3,41 @@
   @import '../assets/_utility-classes.scss';
   @import '../assets/_base.scss';
 
+  :root {
+    --vh: 1vh;
+  }
+
+  .ui-app {
+    height: 100vh;
+    height: calc(var(--vh, 1vh) * 100);
+    @include scrollbar();
+  }
+
 </style>
 
 <template>
-  <div class="ui-app full-screen scroll-y">
+  <div class="ui-app scroll-y">
     <slot></slot>
   </div>
 </template>
 
 <script>
-  export default {
-    name: "ui-app"
-  }
+export default {
+  name: "ui-app",
+  mounted() {
+    this.updateViewportHeightUnit();
+    window.addEventListener("resize", this.updateViewportHeightUnit);
+  },
+  destroyed() {
+    window.removeEventListener("resize", this.updateViewportHeightUnit);
+  },
+  methods: {
+    updateViewportHeightUnit() {
+      document.documentElement.style.setProperty(
+        "--vh",
+        `${window.innerHeight * 0.01}px`
+      );
+    },
+  },
+};
 </script>
